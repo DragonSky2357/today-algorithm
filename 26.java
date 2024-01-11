2464f7c73a04d0556ae3144bdbedb8ead03ba98b
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-01-11 09:56:02 +0900

    Create 26.java

---
+++ 26.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static int solution(int[] absolutes, boolean[] signs) {
+        int sum=0;
+
+        for(int i=0;i<absolutes.length;i++){
+            sum+= signs[i]==true ? absolutes[i] : -absolutes[i];
+        }
+
+        return sum;
+    }
+}
