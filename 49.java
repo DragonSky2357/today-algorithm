1a07253b3d9e39926fa5ff75d55a3ddaad1cff04
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-01-17 12:10:06 +0900

    Create 49.java

---
+++ 49.java
@@ -0,0 +1,18 @@
+import java.util.HashSet;
+
+class Solution {
+    public static int[] solution(int[] numbers) {
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0;i<numbers.length;i++){
+            for(int j=i+1;j<numbers.length;j++){
+                set.add(numbers[i]+numbers[j]);
+            }
+        }
+
+        return set.stream()
+                .mapToInt(Integer::intValue)
+                .sorted()
+                .toArray();
+    }
+}
