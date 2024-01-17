89d102e3ca7e481f4d09755a772242328a551690
DragonSky <38320524+DragonSky2357@users.noreply.github.com>
2024-01-17 12:09:13 +0900

    Create 43.java

---
+++ 43.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int solution(String t, String p) {
+        int answer = 0;
+        int length = t.length() - p.length() + 1;
+
+        for(int i=0;i<length;i++){
+            String s = t.substring(i,i+p.length());
+
+            if(Long.parseLong(s) <= Long.parseLong(p)) answer++;
+        }
+        return answer;
+    }
+}
